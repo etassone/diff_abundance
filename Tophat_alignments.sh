b131a53bc1490f61ac600b7c738812619073b725
etassone <erica.tassone@gmail.com>
2016-05-26 09:06:40 -0700

    Script to align fastq to ref. using Tophat

---
+++ Tophat_alignments.sh
@@ -0,0 +1,26 @@
+ #!/bin/bash
+ #Erica Tassone
+ #May 12, 2016
+ 
+ #Align files to reference for differential abundance using TopHat
+ 
+ #type path to Tophat here so you can access it from the script
+ export PATH=$PATH:/data1/etassone/programs/tophat-2.0.14.Linux_x86_64/
+  
+ #Define some variables for Tophat 
+ TRANSCRIPTOME='/path/to/Transcriptome' #Type path to transcriptome here
+ OUT='/path/to/outputs' #Path you'd like the outputs to go
+ RAW='/path/to/RawData' #Path to the raw fastq files that you want to use for alignments
+ 
+#Loop through raw data and take the prefix for each file - should print only ONE per treatment
+for i in `ls $RAW | awk -F "." '{print $1}'|sort|uniq`;
+do
+	if [ -f"i" ]; then
+	#echo $RAW/$i #to test if you're getting the right file names
+	mkdir $OUT/$i #Make the output directory
+	#Run Tophat. Change settings as needed. 
+	tophat --o $OUT/$i -p 6 -G $TRANSCRIPTOME/transcriptome.new.gff $TRANSCRIPTOME/transcriptome.new.fsa $RAW/$i.R1.fastq $RAW/$i.R2.fastq
+	echo Finished alignment of $i
+fi
+done
+
